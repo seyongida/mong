7d0f7a347ed8b96c438ae19cf22be27064a18bbe
seyong <seyongida@gmail.com>
2023-09-20 16:11:13 +0900

    fix bug

--- mysite/settings.py
+++ mysite/settings.py
@@ -39,9 +39,9 @@ SECRET_KEY = get_secret("SECRET_KEY")
 
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = ['127.0.0.1', '.pythonanywhere.com']
+ALLOWED_HOSTS = ['.pythonanywhere.com']
 
 
 # Application definition
